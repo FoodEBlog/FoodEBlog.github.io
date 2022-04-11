73d348d5ffbc5d22bffcde83bb35f54db69e89f7
FoodEBlog <103253942+FoodEBlog@users.noreply.github.com>
2022-04-11 08:50:28 +0700

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,11 @@
+let counter = 0
+
+const counterparagraph = 
+    document.getElementById("c")
+
+function increment(){
+    console.log("asfadasdwafsa")
+    counter = counter + 1
+    counterparagraph.textContent = counter
+}
+
